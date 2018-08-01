6f737460b6c9505847e8c50797311c2a9ccbc003
James <JLE.Engineer@gmail.com>
2018-08-01 01:33:03 -0400

    Legislative gui done
    Progress!

--- app/src/main/java/pro/jamesegger/americanbureaucracy/legislativeRelated.java
+++ app/src/main/java/pro/jamesegger/americanbureaucracy/legislativeRelated.java
@@ -2,10 +2,11 @@ package pro.jamesegger.americanbureaucracy;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -13,27 +14,25 @@ public class legislativeRelated extends AppCompatActivity
 {
 	List<MyModel> jsonFileScannedIn;
 
-	TableLayout entireTableViewReps;
-	TableRow tableRow1;
-	TableRow tableRow2;
-	TableRow tableRow3;
-	TableRow tableRow4;
-	TableRow tableRow5;
-	TableRow tableRow6;
-	TableRow tableRow7;
-	TableRow tableRow8;
-	TableRow tableRow9;
-	TableRow tableRow10;
+	ImageView redSenateBackgroundSolid;
+    ImageView blueSenateBackgroundSolid;
+    ImageView yellowSenateBackgroundSolid;
 
-	TableLayout entrieTableViewSen;
-	TableRow tableRow1Sen;
-	TableRow tableRow2Sen;
-	TableRow tableRow3Sen;
-	TableRow tableRow4Sen;
+	ImageView redHouseBackgroundSolid;
+	ImageView blueHouseBackgroundSolid;
+	ImageView yellowHouseBackgroundSolid;
 
-	Button buttons[];
+    TextView demSen;
+	TextView otherSen;
+	TextView repSen;
+	TextView demRep;
+	TextView otherRep;
+	TextView repRep;
+
 	int totalReps=0;
 	int totalSen=0;
+
+	int width=0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -44,167 +43,181 @@ public class legislativeRelated extends AppCompatActivity
 		localFileReading readSomePolitics = new localFileReading();
 		jsonFileScannedIn =readSomePolitics.localFileReadingJSON(this);
 
+		redHouseBackgroundSolid= findViewById(R.id.redHouseBackground);
+		blueHouseBackgroundSolid= findViewById(R.id.blueHouseBackground);
+		yellowHouseBackgroundSolid= findViewById(R.id.yellowHouseBackground);
 
-		entireTableViewReps = findViewById(R.id.mainTableRepresentatives);
-		entireTableViewReps.setShrinkAllColumns(true);
+		redSenateBackgroundSolid= findViewById(R.id.redSenateBackground);
+		blueSenateBackgroundSolid= findViewById(R.id.blueSenateBackground);
+		yellowSenateBackgroundSolid= findViewById(R.id.yellowSenateBackground);
 
-		 tableRow1= findViewById(R.id.table1);
-		 tableRow2= findViewById(R.id.table2);
-		 tableRow3= findViewById(R.id.table3);
-		 tableRow4= findViewById(R.id.table4);
-		 tableRow5= findViewById(R.id.table5);
-		 tableRow6= findViewById(R.id.table6);
-		 tableRow7= findViewById(R.id.table7);
-		 tableRow8= findViewById(R.id.table8);
-		 tableRow9= findViewById(R.id.table9);
-		 tableRow10= findViewById(R.id.table10);
+		 demSen= findViewById(R.id.demSen);
+		 otherSen= findViewById(R.id.otherSen);
+		 repSen= findViewById(R.id.repSen);
+		 demRep= findViewById(R.id.demRep);
+		 otherRep= findViewById(R.id.otherRep);
+		 repRep= findViewById(R.id.repRep);
 
-		entrieTableViewSen = findViewById(R.id.mainTableSenators);
-		entrieTableViewSen.setShrinkAllColumns(true);
+		DisplayMetrics displayMetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+		width = displayMetrics.widthPixels;
 
-		tableRow1Sen= findViewById(R.id.table1Sen);
-		tableRow2Sen= findViewById(R.id.table2Sen);
-		tableRow3Sen= findViewById(R.id.table3Sen);
-		tableRow4Sen= findViewById(R.id.table4Sen);
-		}
+	}
 
 	protected void onStart()
 	{
 		super.onStart();
 
-		buttons = new Button[jsonFileScannedIn.size()];
 		int democratReps=0;
 		int republicanReps=0;
 		int otherReps=0;
 
 		int democratSen=0;
 		int republicanSen=0;
-		int otherSen=0;
+		int otherSens=0;
 
-		for(int createButtons =0; createButtons<jsonFileScannedIn.size()-100;createButtons++)
+		for(int senOrRepToScanIn =0; senOrRepToScanIn<jsonFileScannedIn.size();senOrRepToScanIn++)
 		{
 
-			int currentTerm = jsonFileScannedIn.get(createButtons).senAndRepterms.size() -1;
+			int currentTerm = jsonFileScannedIn.get(senOrRepToScanIn).senAndRepterms.size() -1;
 
-			if(jsonFileScannedIn.get(createButtons).senAndRepterms.get(currentTerm).type.equals("rep"))
+			if(jsonFileScannedIn.get(senOrRepToScanIn).senAndRepterms.get(currentTerm).type.equals("rep"))
 			{
-				Button myNewButton = new Button(this);
-
 				//Democrat
-				if(jsonFileScannedIn.get(createButtons).senAndRepterms.get(currentTerm).party.equals("Democrat"))
+				if(jsonFileScannedIn.get(senOrRepToScanIn).senAndRepterms.get(currentTerm).party.equals("Democrat"))
 				{
-					myNewButton.setBackgroundResource(R.drawable.roundbuttonblue);
 					democratReps++;
 				}
 
 				//Republican
-				else if(jsonFileScannedIn.get(createButtons).senAndRepterms.get(currentTerm).party.equals("Republican"))
+				else if(jsonFileScannedIn.get(senOrRepToScanIn).senAndRepterms.get(currentTerm).party.equals("Republican"))
 				{
-					myNewButton.setBackgroundResource(R.drawable.roundbuttonred);
 					republicanReps++;
 				}
 
 				//Other
 				else
 				{
-					myNewButton.setBackgroundResource(R.drawable.roundbuttonyellow);
 					otherReps++;
 				}
-
-				myNewButton.setVisibility(View.VISIBLE);
-				buttons[createButtons] = myNewButton;
-
 				totalReps = democratReps+republicanReps+otherReps;
-
-				if((totalReps+1)%10 == 1)
-				{
-					tableRow1.addView(myNewButton);
-				}
-				else if((totalReps+1)%10 == 2)
-				{
-					tableRow2.addView(myNewButton);
-				}
-				else if((totalReps+1)%10 == 3)
-				{
-					tableRow3.addView(myNewButton);
-				}
-				else if((totalReps+1)%10 == 4)
-				{
-					tableRow4.addView(myNewButton);
-				}
-				else if((totalReps+1)%10 == 5)
-				{
-					tableRow5.addView(myNewButton);
-				}
-				else if((totalReps+1)%10 == 6)
-				{
-					tableRow6.addView(myNewButton);
-				}
-				else if((totalReps+1)%10 == 7)
-				{
-					tableRow7.addView(myNewButton);
-				}
-				else if((totalReps+1)%10 == 8)
-				{
-					tableRow8.addView(myNewButton);
-				}
-				else if((totalReps+1)%10 == 9)
-				{
-					tableRow9.addView(myNewButton);
-				}
-				else if((totalReps+1)%10 == 0)
-				{
-					tableRow10.addView(myNewButton);
-				}
 			}
 
 			//Senator
 			else
 			{
-				Button myNewButton = new Button(this);
-
 				//Democrat
-				if(jsonFileScannedIn.get(createButtons).senAndRepterms.get(currentTerm).party.equals("Democrat"))
+				if(jsonFileScannedIn.get(senOrRepToScanIn).senAndRepterms.get(currentTerm).party.equals("Democrat"))
 				{
-					myNewButton.setBackgroundResource(R.drawable.roundbuttonblue);
-					democratSen++;
+                    democratSen++;
 				}
 
 				//Republican
-				else if(jsonFileScannedIn.get(createButtons).senAndRepterms.get(currentTerm).party.equals("Republican"))
+				else if(jsonFileScannedIn.get(senOrRepToScanIn).senAndRepterms.get(currentTerm).party.equals("Republican"))
 				{
-					myNewButton.setBackgroundResource(R.drawable.roundbuttonred);
 					republicanSen++;
 				}
 
 				//Other
 				else
 				{
-					myNewButton.setBackgroundResource(R.drawable.roundbuttonyellow);
-					otherSen++;
+					otherSens++;
 				}
 
-				myNewButton.setVisibility(View.VISIBLE);
-				buttons[createButtons] = myNewButton;
-
-				totalSen = democratSen+republicanSen+otherSen;
-
-				if((totalSen+1)%4 == 1)
-				{
-					tableRow1Sen.addView(myNewButton);
-				}
-				else if((totalSen+1)%4 == 2)
-				{
-					tableRow2Sen.addView(myNewButton);
-				}
-				else if((totalSen+1)%4 == 3)
-				{
-					tableRow3Sen.addView(myNewButton);
-				}
-				else if((totalSen+1)%4 == 0) {
-					tableRow4Sen.addView(myNewButton);
-				}
+				totalSen = democratSen+republicanSen+otherSens;
 			}
 		}
-	}
+
+        float newDemensionsForSenateRep = (republicanSen * width)/100;
+		float newDemensionsForSenateDem = (democratSen * width)/100;
+		float newDemensionsForSenateOther = (otherSens* width)/100;
+
+		float newDemensionsForHouseRep = (republicanReps * width)/435;
+		float newDemensionsForHouseDem = (democratReps * width)/435;
+		float newDemensionsForHouseOther = (otherReps * width)/435;
+
+		if((int)newDemensionsForHouseDem!=0)
+		{
+			blueHouseBackgroundSolid.setVisibility(View.VISIBLE);
+
+		}
+		else{
+			blueHouseBackgroundSolid.setVisibility(View.INVISIBLE);
+
+		}
+
+		if((int)newDemensionsForHouseOther!=0)
+		{
+			yellowHouseBackgroundSolid.setVisibility(View.VISIBLE);
+
+		}
+		else
+		{
+			yellowHouseBackgroundSolid.setVisibility(View.INVISIBLE);
+
+		}
+
+		if((int)newDemensionsForHouseRep!=0)
+		{
+			redHouseBackgroundSolid.setVisibility(View.VISIBLE);
+
+		}
+		else
+		{
+			redHouseBackgroundSolid.setVisibility(View.INVISIBLE);
+
+		}
+
+		if((int)newDemensionsForSenateDem!=0)
+		{
+			blueSenateBackgroundSolid.setVisibility(View.VISIBLE);
+
+		}
+		else
+		{
+			blueSenateBackgroundSolid.setVisibility(View.INVISIBLE);
+
+		}
+
+		if((int)newDemensionsForSenateOther!=0)
+		{
+			yellowSenateBackgroundSolid.setVisibility(View.VISIBLE);
+
+		}
+
+		else{
+			yellowSenateBackgroundSolid.setVisibility(View.INVISIBLE);
+
+		}
+
+		if((int)newDemensionsForSenateRep!=0)
+		{
+			redSenateBackgroundSolid.setVisibility(View.VISIBLE);
+		}
+
+		else
+		{
+			redSenateBackgroundSolid.setVisibility(View.INVISIBLE);
+
+		}
+
+
+		//35 left 35 right 30 in center.
+        redSenateBackgroundSolid.getLayoutParams().width =(int) newDemensionsForSenateRep;
+        blueSenateBackgroundSolid.getLayoutParams().width =(int)newDemensionsForSenateDem;
+		yellowSenateBackgroundSolid.getLayoutParams().width = (int)newDemensionsForSenateOther;
+
+		redHouseBackgroundSolid.getLayoutParams().width = (int)newDemensionsForHouseRep;
+		blueHouseBackgroundSolid.getLayoutParams().width =(int)newDemensionsForHouseDem;
+		yellowHouseBackgroundSolid.getLayoutParams().width = (int)newDemensionsForHouseOther;
+
+		repRep.setText("Republicans: "+Integer.toString(republicanReps));
+		demRep.setText("Democrats: "+Integer.toString(democratReps));
+		otherRep.setText("Other: " +Integer.toString(otherReps));
+
+		repSen.setText("Republicans: "+Integer.toString(republicanSen));
+		demSen.setText("Democrats: "+Integer.toString(democratSen));
+		otherSen.setText("Other: " +Integer.toString(otherSens));
+
+    }
 }
